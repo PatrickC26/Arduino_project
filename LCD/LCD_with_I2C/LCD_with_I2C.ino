6179caaf9f5e8f2e8ded968e949dcae7a605232e
Patrick Chen <patrick910826@gmail.com>
2022-08-31 21:48:59 +0800

    Code update on 0831

--- LCD/LCD_with_I2C/LCD_with_I2C.ino
+++ LCD/LCD_with_I2C/LCD_with_I2C.ino
@@ -7,12 +7,12 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // 設定 LCD I2
     Serial.begin(9600);    
     lcd.begin(16, 2);      // 初始化 LCD，一行 16 的字元，共 2 行，預設開啟背光
     Serial.println("START");
-    for(int i = 0; i < 3; i++) {
-        lcd.backlight(); // 開啟背光
-        delay(250);
-        lcd.noBacklight(); // 關閉背光
-        delay(250);
-      }
+//    for(int i = 0; i < 3; i++) {
+//        lcd.backlight(); // 開啟背光
+//        delay(250);
+//        lcd.noBacklight(); // 關閉背光
+//        delay(250);
+//      }
       lcd.backlight();
       lcd.setCursor(0,0); // set cursor at first line first alphabet
       lcd.print("Initializing");
